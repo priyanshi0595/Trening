20625823a9beb4542b0a09cb0fb07c32876b28cf
bhavsar priyanshi <bhavsarpriyanshi05@gmail.com>
2026-01-11 15:09:06 +0530

    python microsoft trening

---
+++ demo1.py
@@ -0,0 +1,12 @@
+print('this is a demo Python script.')
+#global enveronment
+import numpy  as np
+print(np.__version__)
+import sklearn  
+print(sklearn.__version__)
+#priyanshi
+#virtual eniveroment
+#python -m venv en # type in terminal
+
+#en/Script/activate #type in terminal for  activate 
+
